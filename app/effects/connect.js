fd393fa4ca49eecb9ea9ffc428034820257a47b0
Michael Williams <michael.williams@enspiral.com>
2016-06-17 03:10:34 +1200

    reconnect client with `pull-next`

--- app/effects/connect.js
+++ app/effects/connect.js
@@ -4,6 +4,7 @@ const { assign } = require('lodash')
 const ws = require('pull-ws-server')
 const cat = require('pull-cat')
 const pullJson = require('pull-json-doubleline')
+const pullNext = require('pull-next')
 
 const Action = require('../types/action')
 const Set = require('../actions/set')
@@ -14,25 +15,27 @@ const Connect = Tc.struct({
 
 Connect.prototype.run = function runConnect (streams) {
   const effect = this
+  
+  return pullNext(function () {
+    const client = ws.connect(effect.url)
 
-  const client = ws.connect(effect.url)
+    pull(
+      streams.actions(),
+      pull.filterNot(Set.is),
+      pull.map((action) => {
+        const Type = Action.dispatch(action)
+        return assign({ type: Type.meta.name }, action)
+      }),
+      pullJson(client.sink)
+    )
 
-  pull(
-    streams.actions(),
-    pull.filterNot(Set.is),
-    pull.map((action) => {
-      const Type = Action.dispatch(action)
-      return assign({ type: Type.meta.name }, action)
-    }),
-    pullJson(client.sink)
-  )
-
-  return pull(
-    pullJson(client.source),
-    pull.map((model) => {
-      return Set({ model })
-    })
-  )
+    return pull(
+      pullJson(client.source),
+      pull.map((model) => {
+        return Set({ model })
+      })
+    )
+  })
 }
 
 module.exports = Connect
