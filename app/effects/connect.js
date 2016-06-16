cd2c2c47ede99090b3a8f56e0b333512b96cd6e6
Michael Williams <michael.williams@enspiral.com>
2016-06-17 03:10:34 +1200

    reconnect client with `pull-next`

--- app/effects/connect.js
+++ app/effects/connect.js
@@ -4,6 +4,7 @@ const { assign } = require('lodash')
 const ws = require('pull-ws-server')
 const cat = require('pull-cat')
 const pullJson = require('pull-json-doubleline')
+const pullNext = require('pull-next')
 
 const actions = require('../actions')
 const Action = require('../types/action')
@@ -14,29 +15,31 @@ const Connect = Tc.struct({
 
 Connect.prototype.run = function runConnect (sources) {
   const effect = this
+  
+  return pullNext(function () {
+    const server = ws.connect(effect.url)
 
-  const server = ws.connect(effect.url)
-
-  const client = {
-    source: pull(
-      sources.actions(),
-      pull.filterNot((action) => {
-        return actions.Set.is(action) || actions.Patch.is(action)
-      }),
-      pull.map((action) => {
-        const Type = Action.dispatch(action)
-        return assign({ type: Type.meta.name }, action)
+    const client = {
+      source: pull(
+        sources.actions(),
+        pull.filterNot((action) => {
+          return actions.Set.is(action) || actions.Patch.is(action)
+        }),
+        pull.map((action) => {
+          const Type = Action.dispatch(action)
+          return assign({ type: Type.meta.name }, action)
+        })
+      ),
+      through: pull.map((action) => {
+        const Type = actions[action.type]
+        return Type(action)
       })
-    ),
-    through: pull.map((action) => {
-      const Type = actions[action.type]
-      return Type(action)
-    })
-  }
+    }
 
-  pull(client.source, pullJson(server.sink))
+    pull(client.source, pullJson(server.sink))
 
-  return pull(pullJson(server.source), client.through)
+    return pull(pullJson(server.source), client.through)
+  })
 }
 
 module.exports = Connect
